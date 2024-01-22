8c8fa8679b003f7d134161fc635c3d0fdfd2f44a
Dion Dijkhuizen <diondijkhuizen1990@hotmail.com>
2024-01-22 19:33:43 +0100

    les5

---
+++ les 5/oefen.py
@@ -0,0 +1,14 @@
+a = "maandag"
+b = "dinsdag"
+c = "woensdag"
+print(a,b,c, sep="-")
+
+for d in range (10, 0, -1):
+    print(d)
+print("launge")
+
+e = 0
+while e < 6:
+    print(e)
+    e = e + 1 
+print("eind")
